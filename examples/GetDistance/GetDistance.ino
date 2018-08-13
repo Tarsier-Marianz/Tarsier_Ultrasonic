8905c902e3161e71f8d2c32a074f25e47b50e0da
Tarsier-Marianz <36526792+Tarsier-Marianz@users.noreply.github.com>
2018-08-13 23:04:45 +0800

    *pin name declaration in example

--- examples/GetDistance/GetDistance.ino
+++ examples/GetDistance/GetDistance.ino
@@ -1,16 +1,17 @@
 #include "Tarsier_Ultrasonic.h"
 
-Tarsier_Ultrasonic _hcSR04_Sensor(6, 7);
+const int TRIG_PIN    = 6;
+const int ECHO_PIN    = 7;
 
-void setup()
-{
-    Serial.begin(9600);
-    //_hcSR04_Sensor.setPins(6, 7);     // alternative way of pin configuration
+Tarsier_Ultrasonic _hcSR04_Sensor(TRIG_PIN, ECHO_PIN);
+
+void setup() {
+  Serial.begin(9600);
+  //_hcSR04_Sensor.setPins(TRIG_PIN, ECHO_PIN);     // alternative way of pin configuration
 }
 
-void loop()
-{
-    long distanceCm = _hcSR04_Sensor.getDistanceCm();
-    Serial.print("Distance in Centimeter: ");
-    Serial.println(distanceCm);
+void loop() {
+  long distanceCm = _hcSR04_Sensor.getDistanceCm();
+  Serial.print("Distance in Centimeter: ");
+  Serial.println(distanceCm);
 }
