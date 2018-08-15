f3fbd76f0a66968e609707f21d54e894a7df21ac
Tarsier-Marianz <36526792+Tarsier-Marianz@users.noreply.github.com>
2018-08-15 09:21:54 +0800

    *declaration of LCD pin configuration

--- examples/GetDistanceLCD/GetDistanceLCD.ino
+++ examples/GetDistanceLCD/GetDistanceLCD.ino
@@ -1,12 +1,19 @@
-
 #include "Tarsier_Ultrasonic.h"
 #include "LiquidCrystal.h"
 
-const int TRIG_PIN    = 6;
-const int ECHO_PIN    = 7;
+//ultrasonic pin configuration
+const int TRIG_PIN  = 6;
+const int ECHO_PIN  = 7;
+//LCD pin configuration
+const int RS_PIN    = 12;
+const int EN_PIN    = 11;
+const int D4_PIN    = 5;
+const int D5_PIN    = 4;
+const int D6_PIN    = 3;
+const int D7_PIN    = 2;
 
 Tarsier_Ultrasonic _hcSR04_Sensor(TRIG_PIN, ECHO_PIN);
-LiquidCrystal _lcd(12, 11, 5, 4, 3, 2);
+LiquidCrystal _lcd(RS_PIN, EN_PIN, D4_PIN, D5_PIN, D6_PIN, D7_PIN);
 
 void setup() {
   _lcd.begin(20, 4);
